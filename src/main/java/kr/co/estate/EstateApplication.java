429453a91e6e996a41e1055b7c09c578c0b92613
chaehun <youspend8@gmail.com>
2020-12-07 17:42:24 +0900

    feat : OpenApiProperties 클래스 추가 및 init schema sql 추가

--- src/main/java/kr/co/estate/EstateApplication.java
+++ src/main/java/kr/co/estate/EstateApplication.java
@@ -1,11 +1,14 @@
 package kr.co.estate;
 
+import kr.co.estate.config.properties.OpenApiProperties;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableConfigurationProperties(value = {OpenApiProperties.class})
 public class EstateApplication {
 
     public static void main(String[] args) {
