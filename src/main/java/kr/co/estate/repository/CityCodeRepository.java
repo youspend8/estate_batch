356523f28d030a0ff8da5d97e6f6fe5c496d3838
chaehun <youspend8@gmail.com>
2020-12-10 17:39:46 +0900

    feat : CityCodeRepository findByRegionAndSigunguAndFullNameLike 추가

--- src/main/java/kr/co/estate/repository/CityCodeRepository.java
+++ src/main/java/kr/co/estate/repository/CityCodeRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CityCodeRepository extends JpaRepository<CityCodeEntity, String> {
     List<CityCodeEntity> findAllByType(String type);
     List<CityCodeEntity> findAllByNameIn(List<String> list);
-    CityCodeEntity findByRegionAndSigunguAndName(String region, String sigungu, String name);
+    Optional<CityCodeEntity> findByRegionAndSigunguAndFullnameLike(String region, String sigungu, String name);
 }
