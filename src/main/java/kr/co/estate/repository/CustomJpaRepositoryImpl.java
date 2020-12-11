dd4186d6bc93520da02204cbaac104452f758d99
chaehun <youspend8@gmail.com>
2020-12-11 17:49:22 +0900

    feat : batch insert logging 추가

--- src/main/java/kr/co/estate/repository/CustomJpaRepositoryImpl.java
+++ src/main/java/kr/co/estate/repository/CustomJpaRepositoryImpl.java
@@ -3,6 +3,7 @@ package kr.co.estate.repository;
 import kr.co.estate.config.UniqueIdGenerator;
 import kr.co.estate.entity.TradeMasterEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CustomJpaRepositoryImpl implements CustomJpaRepository<TradeMasterEntity> {
     private final JdbcTemplate jdbcTemplate;
 
@@ -46,38 +48,42 @@ public class CustomJpaRepositoryImpl implements CustomJpaRepository<TradeMasterE
                 "   ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, current_timestamp(), POINT(?, ?), ?, ?, ?, ?" +
                 ")";
 
-        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                TradeMasterEntity tradeMasterEntity = list.get(i);
-                ps.setObject(1, UniqueIdGenerator.generate());
-                ps.setObject(2, tradeMasterEntity.getDeal().getDealYear());
-                ps.setObject(3, tradeMasterEntity.getDeal().getDealMonth());
-                ps.setObject(4, tradeMasterEntity.getDeal().getDealDay());
-                ps.setObject(5, tradeMasterEntity.getLocation().getDong());
-                ps.setObject(6, tradeMasterEntity.getLocation().getJibun());
-                ps.setObject(7, tradeMasterEntity.getBuildYear());
-                ps.setObject(8, tradeMasterEntity.getLocation().getSigungu());
-                ps.setObject(9, tradeMasterEntity.getFloor());
-                ps.setObject(10, tradeMasterEntity.getArea());
-                ps.setObject(11, tradeMasterEntity.getAreaSub());
-                ps.setObject(12, tradeMasterEntity.getAmount());
-                ps.setObject(13, tradeMasterEntity.getAmountOption());
-                ps.setObject(14, tradeMasterEntity.getTradeType().ordinal());
-                ps.setObject(15, tradeMasterEntity.getName());
-                ps.setObject(16, tradeMasterEntity.getVillaType());
-                ps.setObject(17, tradeMasterEntity.getPoint().getX());
-                ps.setObject(18, tradeMasterEntity.getPoint().getY());
-                ps.setObject(19, tradeMasterEntity.getDeal().getDealDate().toString());
-                ps.setObject(20, tradeMasterEntity.getLocation().getRegionCode());
-                ps.setObject(21, tradeMasterEntity.getLocation().getSigunguCode());
-                ps.setObject(22, tradeMasterEntity.getLocation().getUmdCode());
-            }
+        try {
+            jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    TradeMasterEntity tradeMasterEntity = list.get(i);
+                    ps.setObject(1, UniqueIdGenerator.generate());
+                    ps.setObject(2, tradeMasterEntity.getDeal().getDealYear());
+                    ps.setObject(3, tradeMasterEntity.getDeal().getDealMonth());
+                    ps.setObject(4, tradeMasterEntity.getDeal().getDealDay());
+                    ps.setObject(5, tradeMasterEntity.getLocation().getDong());
+                    ps.setObject(6, tradeMasterEntity.getLocation().getJibun());
+                    ps.setObject(7, tradeMasterEntity.getBuildYear());
+                    ps.setObject(8, tradeMasterEntity.getLocation().getSigungu());
+                    ps.setObject(9, tradeMasterEntity.getFloor());
+                    ps.setObject(10, tradeMasterEntity.getArea());
+                    ps.setObject(11, tradeMasterEntity.getAreaSub());
+                    ps.setObject(12, tradeMasterEntity.getAmount());
+                    ps.setObject(13, tradeMasterEntity.getAmountOption());
+                    ps.setObject(14, tradeMasterEntity.getTradeType().ordinal());
+                    ps.setObject(15, tradeMasterEntity.getName());
+                    ps.setObject(16, tradeMasterEntity.getVillaType());
+                    ps.setObject(17, tradeMasterEntity.getPoint().getX());
+                    ps.setObject(18, tradeMasterEntity.getPoint().getY());
+                    ps.setObject(19, tradeMasterEntity.getDeal().getDealDate().toString());
+                    ps.setObject(20, tradeMasterEntity.getLocation().getRegionCode());
+                    ps.setObject(21, tradeMasterEntity.getLocation().getSigunguCode());
+                    ps.setObject(22, tradeMasterEntity.getLocation().getUmdCode());
+                }
 
-            @Override
-            public int getBatchSize() {
-                return list.size();
-            }
-        });
+                @Override
+                public int getBatchSize() {
+                    return list.size();
+                }
+            });
+        } catch (Exception e) {
+            log.error("Insert 오류 발생 ==> {}", e.getMessage());
+        }
     }
 }
